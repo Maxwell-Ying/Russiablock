1c848cab4077c860c31e2e32b076cce6ab1307d4
Maxwell-Ying <952167878@qq.com>
2019-04-23 11:51:52 +0800

    finish main function.

--- box.cpp
+++ box.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 
 #include "box.hpp"
 
@@ -6,22 +7,40 @@ using std::cout;
 using std::endl;
 
 // func for Box
-void Box::draw_map() {
-  reset_pos();
+// print func, to see borad and items.
+void Box::print(Map &m) {
   cout << "+------------+" << endl;
-  for (const auto & i : map) {
+  for (const auto & i : m) {
     cout << "|";
     for (const auto & j : i) {
       if (j) {
         cout << "x";
       } else {
-        cout << "o";
+        cout << " ";
       }
     }
     cout << "|";
     cout << endl;
   }
-  cout << "+------------" << endl;
+  cout << "+------------+" << endl;
+}
+
+// draw box and the data fixed in box
+// currently not use in this project.
+void Box::draw_map() {
+  reset_pos();
+  print(map);
+}
+
+// draw map and block in map, 
+void Box::draw_map(Position pos, array<int, 6> points) {
+  Map tmp = map;
+  tmp[pos.first][pos.second] = 1;
+  tmp[pos.first+points[0]][pos.second+points[1]] = 1;
+  tmp[pos.first+points[2]][pos.second+points[3]] = 1;
+  tmp[pos.first+points[4]][pos.second+points[5]] = 1;
+  reset_pos();
+  print(tmp);
 }
 
 bool Box::check_line_full(int lino) {
@@ -34,27 +53,34 @@ bool Box::check_line_full(int lino) {
   return true;
 }
 
+// erase a full line, assume check_line_full is already returned true
 void Box::erase_line(int lino) {
-  if (lino < 0 || lino >= 12) {
+  if (lino < 0 || lino >= 23) {
     return ;
   }
-  for (; lino<11; lino++) {
-    map[lino] = map[lino+1];
+  for (; lino>0; lino--) {
+    map[lino] = map[lino-1];
   }
-  map[11].fill(0);
+  // fill top with 0 is needed
+  map[0].fill(0);
 }
 
-int Box::check_erase() {
-  for (auto i=0; i<12; i++) {
+// check if there is full lines
+// there can be multi line full so an array is needed.
+array<int, 23> Box::check_erase() {
+  array<int, 23> ret;
+  ret.fill(0);
+  for (auto i=0; i<23; i++) {
     if (check_line_full(i)) {
-      return i;
+      ret[i] = 1;
     }
   }
-  return -1;
+  return ret;
 }
 
+// check if a position in box is empty.
 bool Box::fit(const Position & p) {
-  if (p.first >= 0 && p.first < map.size() && p.second >= 0 && p.second < map[0].size) {
+  if (p.first >= 0 && p.first < map.size() && p.second >= 0 && p.second < map[0].size()) {
     return map[p.first][p.second] == 0;
   }
   if (p.first == -1 && p.second >= 0 && p.second < map[0].size()) {
@@ -63,7 +89,8 @@ bool Box::fit(const Position & p) {
   return false;
 }
 
-bool Box::fit(array<int, 6> &s, const Position& pos) {
+// check if a shape in pos can be place to box
+bool Box::fit(array<int, 6> s, Position pos) {
   Position pos1{pos.first, pos.second};
   Position pos2{pos.first + s[0], pos.second + s[1]};
   Position pos3{pos.first + s[2], pos.second + s[3]};
@@ -71,15 +98,21 @@ bool Box::fit(array<int, 6> &s, const Position& pos) {
   return fit(pos1) && fit(pos2) && fit(pos3) && fit(pos4);
 }
 
+// set a position occupied, used in merge origin items and new block
+void Box::set_bit(Position p) {
+  map[p.first][p.second] = 1;
+} 
+
 
 // func for block
-array<int, 6> Block::pre_rotate_state() {
+// if before == true, return current state, else return rotated state
+array<int, 6> Block::pre_rotate_state(bool before = false) {
   // rotate time by different shape
-  // do 2 or 3 times rotate can result in 4 different shape of I
+  // do 2 or 3 times rotate can result in 4 different shape of I,S,Z
   // and that is not what I want
-  int r_time = rotate + 1;
-  if (shape == shapeT) {
-    // only T have exactly 4 shape
+  int r_time = before ? rotate: rotate + 1;
+  if (shape == shapeT || shape == shapeL || shape == shapeJ) {
+    // only TLJ have exactly 4 shape
     r_time %= 4;
   } else if (shape == shapeO) {
     // O have only one shape
@@ -89,32 +122,123 @@ array<int, 6> Block::pre_rotate_state() {
   }
 
   array<int, 6> pre = {bitmap[shape]};
+  // flip along y=-x then x=0 rotate the shape by 90 degree.
   for (auto i=0; i<r_time; i++) {
-    pre = array<int, 6>()
+    pre = array<int, 6>{pre[1]*-1, pre[0], pre[3]*-1, pre[2], pre[5]*-1, pre[4]};
+  }
+  return pre;
+}
+
+// check if block can rotate in this pos
+bool Block::can_rotate() {
+  return box.fit(pre_rotate_state(false), pos);
+}
+
+// do rotate, simply add rotate by 1
+void Block::do_rotate() {
+  rotate += 1;
+  if (shape == shapeT) {
+    rotate %= 4;
+  } else if (shape == shapeO) {
+    rotate = 0;
+  } else {
+    rotate %= 2;
   }
 }
 
-bool Block::can_rotate() {
-  return box.fit(rotated())
+// check movement
+bool Block::can_move_left() {
+  return box.fit(pre_rotate_state(true), Position{pos.first, pos.second-1});
+}
+
+// do movement
+void Block::do_move_left() {
+  pos.second -= 1;
+}
+
+bool Block::can_move_right() {
+  return box.fit(pre_rotate_state(true), Position{pos.first, pos.second+1});
+}
+
+void Block::do_move_right() {
+  pos.second += 1;
+}
+
+bool Block::can_fall() {
+  return box.fit(pre_rotate_state(true), Position{pos.first + 1, pos.second});
+}
+
+void Block::do_fall() {
+  pos.first += 1;
+}
+
+// reset pos to top for new block
+void Block::reset_pos() {
+  pos.first = 0;
+  pos.second = 6;
+}
+
+void Block::set_shape(Shape s) {
+  shape = s;
+}
+
+// to fix block to box, after this the block need to be reset
+void Block::merge() {
+  auto bits = pre_rotate_state(true);
+  box.set_bit(pos);
+  box.set_bit({pos.first+bits[0], pos.second+bits[1]});
+  box.set_bit({pos.first+bits[2], pos.second+bits[3]});
+  box.set_bit({pos.first+bits[4], pos.second+bits[5]});
+}
+
+void Block::draw_block_and_map() {
+  box.draw_map(pos, pre_rotate_state(true));
 }
 
 // func for Game
+// draw func, there should also be some thing like next block
+// score and comic character. this will be in following update.
+void Game::draw_map() {
+  block.draw_block_and_map();
+  cout << debug_info << endl;
+}
+
+// for auto fall. if count > count_max then do fall.
+void Game::time_pass(int i) {
+  time_count += interval;
+  if (time_count >= time_count_max) {
+    try_fall();
+    time_count = 0;
+  }
+}
+
+// main loop for game. catch keyboard input and react.
 void Game::run() {
+  init_cursor();
+  draw_map();
   while (1) {
-    char ch = kbhit();
-    if (ch) {
+    // this draw seem to be too frequent and useless.
+    // draw every movement or erase instead.
+    // draw_map();
+    char ch;
+    if (_kbhit()) {
+      ch = _getch();
       switch (ch)
       {
       case 'w':
+        debug_info = "try_rotate";
         try_rotate();
         break;
       case 'a':
+        debug_info = "try_left";
         try_move_left();
         break;
       case 'd':
+        debug_info = "try_right";
         try_move_right();
         break;
       case 's':
+        debug_info = "try_fall";
         try_fall();
         time_count = 0;
         break;
@@ -122,20 +246,75 @@ void Game::run() {
         break;
       }
     }
+    Sleep(interval);
     time_pass(interval);
   }
 }
 
-int Game::check_erase() {
+array<int, 23> Game::check_erase() {
   return box.check_erase();
 }
 
+// erase func. remember the array returned is current line and 
+// when one line is deleted, all lines above moved down.
+// so erase_count is need or any other way to do so.
 void Game::erase() {
-  int erase = check_erase();
-  if (erase >=0 && erase < 12) {
-    box.erase_line(erase);
+  array<int, 23> erase = check_erase();
+  int erase_count = 0;
+  for (int i=22; i>=0; i--) {
+    if (erase[i]) {
+      box.erase_line(i+erase_count);
+      erase_count += 1;
+    }
   }
   draw_map();
+}
+
+// try func do not exactly do operation.
+// only do if can do.
+// draw map whether or not really do.
+void Game::try_rotate() {
+  if (block.can_rotate()) {
+    block.do_rotate();
+  }
+  draw_map();
+}
+
+void Game::try_move_left() {
+  if (block.can_move_left()) {
+    block.do_move_left();
+  }
+  draw_map();
+}
+
+void Game::try_move_right() {
+  if (block.can_move_right()) {
+    block.do_move_right();
+  }
+  draw_map();
+}
+
+void Game::try_fall() {
+  if (block.can_fall()) {
+    block.do_fall();
+  } else {
+    // these is all code for  new block.
+    block.merge();
+    block.reset_pos();
+    block.set_shape(next_shape);
+    next_shape = get_next();
+    erase();
+  }
+  draw_map();
+}
+
+// get next shape. remember to make the two item static 
+// to get "really" random shape.
+Shape Game::get_next() {
+  static std::uniform_int_distribution<unsigned> u(0, 6);
+  static std::default_random_engine e;
+  unsigned int tmp = u(e);
+  return static_cast<Shape>(tmp);
 }
 
 int main(int argc, char * argv[]) {
--- box.hpp
+++ box.hpp
@@ -4,32 +4,45 @@
 #include <windows.h>
 #include <conio.h>
 #include <array>
+#include <string>
 
 using std::array;
 using std::pair;
+using std::string;
 
 array<array<int, 6>, 7> bitmap{
-  {-1, 0, 1, 0, 2, 0},  // I
-  // {-1, 0, 1, 0, 1, -1}, // J
-  // {-1, 0, 1, 0, 1, 1},  // L
-  // {0, 1, 1, 0, 1, 1},   // O
-  // {0, 1, 1, 0, 1, -1},  // S
-  // {1, 0, 0, 1, 0, -1},  // T
-  // {0, -1, 1, 0, 1, 1},  // Z
+  {-1,  0, 1, 0, 2,  0,  // I
+   -1,  0, 1, 0, 1, -1, // J
+   -1,  0, 1, 0, 1,  1,  // L
+    0,  1, 1, 0, 1,  1,   // O
+    0,  1, 1, 0, 1, -1,  // S
+    1,  0, 0, 1, 0, -1,  // T
+    0, -1, 1, 0, 1,  1,}  // Z
 };
 
 typedef array<array<int, 12>, 23> Map;
 typedef pair<int, int> Position;
 
+// set cursor to needed place.
 void set_pos(short x, short y) {
   COORD pos{x, y};
   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); 
 }
 
+// make cursor invisible. set size is needed or this doesnot work.
+void init_cursor() {
+  _CONSOLE_CURSOR_INFO ccinfo;
+  ccinfo.bVisible = false;
+  ccinfo.dwSize = 1;
+  SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ccinfo);
+}
+
+// set pos to left up for new draw.
 void reset_pos() {
   set_pos(0, 0);
 }
 
+// shape. order is respective to bitmap
 enum Shape {
   shapeI, 
   shapeJ,
@@ -41,6 +54,7 @@ enum Shape {
 };
 
 class Box {
+  // board
   Map map;
   
   bool fit(const Position &);
@@ -52,46 +66,71 @@ public:
   }
 
   void draw_map();
+  void draw_map(Position, array<int, 6>);
   bool check_line_full(int lino);
   void erase_line(int lino);
-  int check_erase();
-  bool fit(array<int, 6> &, const Position&);
+  array<int, 23> check_erase();
+  bool fit(array<int, 6> , Position);
+  void set_bit(Position);
+  void print(Map &m);
 };
 
 class Block {
-  Position pos{23,6};
-  Shape shape{shapeI};
+  // center of block. with 3 points refered by shape and bitmap
+  Position pos{0,6};
+  Shape shape;
+  // rotate state of shape, only really rotate when check or draw.
   int rotate{0};
+  // a block need a box to validate movement.
   Box & box;
 public:
-  Block(Box& _box, Shape _shape = shapeI) : box(_box), shape(_shape) {}
-  array<int, 6> pre_rotate_state();
+  Block(Box& _box, Shape _shape = shapeT) : box(_box), shape(_shape) {}
+  array<int, 6> pre_rotate_state(bool before);
   bool can_rotate();
   void do_rotate();
+  bool can_move_left();
+  void do_move_left();
+  bool can_move_right();
+  void do_move_right();
+  bool can_fall();
+  void do_fall();
+  void merge();
+  void reset_pos();
+  void set_shape(Shape);
+  void draw_block_and_map();
 };
 
 class Game {
   Box box;
   Block block{box};
+  // next shape of block, can also place to block class
+  Shape next_shape{shapeT};
+  string debug_info;
+  // score
   int score = 0;
   int level = 1;
   int time_count = 0;
+  int time_count_max = 1000;
+  // interval between two kb check. decrease to get high presion
+  // increse to get better performance.
   static int interval;
 
 public:
   Game() = default;
 
-  void draw_map() {}
+  void draw_map();
   void time_pass(int i);
-  int check_erase();
+  array<int, 23> check_erase();
   void erase();
   void run();
   void try_rotate();
   void try_move_right();
   void try_move_left();
   void try_fall();
+  Shape get_next();
 };
 
+// init for static item.
 int Game::interval = 5;
 
 #endif
